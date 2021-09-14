fec6f62d188fa020095124a379734c7af7ac8f9b
Lindsey Baker <bakerlindseymarie@gmail.com>
2021-09-14 12:41:57 -0700

    Add test to check creation of 3 word phrase table

---
+++ __tests__/index.test.js
@@ -0,0 +1,10 @@
+import Phrases from './index.js'
+
+describe('Phrases', () => {
+
+    test('should correctly create a table with 43 phrases', () => {
+        const textSample = "This is a phrase. I am now going to mess with the repitition. Now this is also a phrase. Three word phrase. What is I add punctuation 'Three word phrase'. Now I am going to have a odd case tHree WoRD pHRase! So many letters.";
+
+      });
+
+});
