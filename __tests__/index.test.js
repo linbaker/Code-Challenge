360a2be87fe7a0e7aa1383f88d427f8cfb328b73
Lindsey Baker <bakerlindseymarie@gmail.com>
2021-09-14 21:10:56 -0700

    Refactor into Phrases class

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,16 +1,18 @@
 require('jest');
-const groupPhrases = require('../index.js');
+const Phrases = require('../index.js');
 
-describe('groupPhrases', () => {
+describe('Phrases', () => {
 
     test('should correctly create a map of thee word phrases', () => {
-        const textSample = "This is a phrase. I am now going to mess with the repitition. Now this is also a phrase. Three word phrase. What is I add punctuation 'Three word phrase'. Now I am going to have a odd case tHree WoRD pHRase! So many letters.";
-        expect(groupPhrases(textSample).size).toEqual(43)
+        const testText = "This is a phrase. I am now going to mess with the repitition. Now this is also a phrase. Three word phrase. What is I add punctuation 'Three word phrase'. Now I am going to have a odd case tHree WoRD pHRase! So many letters.";
+        const phrases = new Phrases(testText)
+        expect(phrases.groupPhrases().size).toEqual(43)
       });
 
     test('should correctly return map with size 1 if text contains only three words', () => {
-        const textSample = "I am three";
-        expect(groupPhrases(textSample).size).toEqual(1)
+        const testText = "I am three";
+        const phrases = new Phrases(testText)
+        expect(phrases.groupPhrases().size).toEqual(1)
     });
     
 
--- index.js
+++ index.js
@@ -2,16 +2,22 @@ const { moduleExpression } = require("@babel/types");
 
 //let textSample = "This is a phrase. I am now going to mess with the repitition. Now this is also a phrase. Three word phrase. What is I add punctuation 'Three word phrase'. Now I am going to have a odd case tHree WoRD pHRase! So many letters."
 
-
-const phrasesMap = new Map();
-
-function groupPhrases (textSample) {
-    let wordArray = textSample.split(' ');
-    for (let i = 0; i < wordArray.length - 2; i++) {
-        let threeWordPhrase = wordArray[i] + ' ' + wordArray[i+1] + ' ' + wordArray[i+2];
-        phrasesMap.set(threeWordPhrase, 1)
+class Phrases {
+    constructor(textSample) {
+        this.textSample = textSample;
+        this.phrasesMap = new Map();
     }
-	return phrasesMap
-};
 
-module.exports = groupPhrases;
+    groupPhrases () {
+        let wordArray = this.textSample.split(' ');
+        for (let i = 0; i < wordArray.length - 2; i++) {
+            let threeWordPhrase = wordArray[i] + ' ' + wordArray[i+1] + ' ' + wordArray[i+2];
+            this.phrasesMap.set(threeWordPhrase, 1)
+        }
+        return this.phrasesMap
+    };
+
+}
+
+
+module.exports = Phrases;
