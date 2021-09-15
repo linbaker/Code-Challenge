9cb0ed515bc9620ea36cc841da94580b87f830a6
Lindsey Baker <bakerlindseymarie@gmail.com>
2021-09-14 20:12:23 -0700

    Pass groupPhrases size test

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-let textSample = "This is a phrase. I am now going to mess with the repitition. Now this is also a phrase. Three word phrase. What is I add punctuation 'Three word phrase'. Now I am going to have a odd case tHree WoRD pHRase! So many letters."
+const { moduleExpression } = require("@babel/types");
 
-let wordArray = textSample.split(' ');
+//let textSample = "This is a phrase. I am now going to mess with the repitition. Now this is also a phrase. Three word phrase. What is I add punctuation 'Three word phrase'. Now I am going to have a odd case tHree WoRD pHRase! So many letters."
 
-console.log(wordArray);
 
-class HashTable {
-    constructor() {
-      this.values = {};
-      this.length =  0;
-      this.size =  0;
+const phrasesMap = new Map();
+
+function groupPhrases (textSample) {
+    let wordArray = textSample.split(' ');
+    for (let i = 0; i < wordArray.length - 2; i++) {
+        let threeWordPhrase = wordArray[i] + ' ' + wordArray[i+1] + ' ' + wordArray[i+2];
+        phrasesMap.set(threeWordPhrase, 1)
     }
-   }
+	return phrasesMap
+};
+
+module.exports = groupPhrases;
