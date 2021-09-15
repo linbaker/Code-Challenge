c34500f3329faef3743ee4ca18a3871b95ba88db
Lindsey Baker <bakerlindseymarie@gmail.com>
2021-09-14 20:03:54 -0700

    Correct 'expect' failure

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,10 +1,11 @@
+require('jest');
 const groupPhrases = require('../index.js');
 
 describe('groupPhrases', () => {
 
     test('should correctly create a map of thee word phrases', () => {
         const textSample = "This is a phrase. I am now going to mess with the repitition. Now this is also a phrase. Three word phrase. What is I add punctuation 'Three word phrase'. Now I am going to have a odd case tHree WoRD pHRase! So many letters.";
-        expect.groupPhrases(textSample).size.toEqual(43)
+        expect(groupPhrases(textSample).size).toEqual(43)
       });
 
 });
